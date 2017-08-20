09bfb850c378879ba12e7d6b55b5c100152180ea
Magnus Glantz <sudo@redhat.com>
2017-08-20 14:47:09 +0200

    Updated rpm prepreqs

--- scripts/bastionPrep.sh
+++ scripts/bastionPrep.sh
@@ -40,7 +40,7 @@ subscription-manager repos \
 # Install base packages and update system to latest packages
 echo $(date) " - Install base packages and update system to latest packages"
 
-yum -y install wget git net-tools bind-utils iptables-services bridge-utils bash-completion httpd-tools
+yum -y install wget git net-tools bind-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct
 yum -y update --exclude=WALinuxAgent
 
 # Install OpenShift utilities
