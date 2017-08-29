98c771381922b82818bb418f58e35abf8190c8e4
Magnus Glantz <sudo@redhat.com>
2017-08-30 00:32:55 +0200

    Smarter script which works OOB after git clone of this repo

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,63 @@
 # Shell script to deploy OpenShift 3.6 on Microsoft Azure
 # Magnus Glantz, sudo@redhat.com, 2017
 
+OK=0
+if [ -f ./deploy.cfg ]; then
+	. ./deploy.cfg
+	if test -z $RHN_ACCOUNT; then
+		OK=1
+	elif test -z $RHN_PASSWORD; then
+		OK=1
+	elif test -z $OCP_USER; then
+		OK=1
+	elif test -z $OCP_PASSWORD; then
+		OK=1
+	elif test -z $SUBSCRIPTION_POOL; then
+		OK=1
+	elif test -z $LOCATION; then
+		OK=1
+	fi
+else
+	OK=1
+fi
+
+if [ "$OK" -eq 1 ]; then
+	echo "Missing variable values: Edit the deploy.cfg file"
+	exit 1
+fi
+
+if [ -f ~/.ssh/id_rsa.pub ]; then
+        PUBLIC_SSH_KEY="$(cat ~/.ssh/id_rsa.pub)"
+	if grep "PUBLIC_SSH_KEY" azuredeploy.parameters.json >/dev/null; then
+		echo "Edit azuredeploy.parameters.json and past your public ssh key into the value for sshPublicKey."
+		echo "Your key is:"
+		echo "$PUBLIC_SSH_KEY"
+		exit 1
+	fi
+else
+        echo "No SSH key found in ~/.ssh/id_rsa. Generating key."
+        ssh-keygen
+	PUBLIC_SSH_KEY="$(cat ~/.ssh/id_rsa)"
+	echo "Edit azuredeploy.parameters.json and past your public ssh key into the value for sshPublicKey.
+"
+	echo "Your key is:"
+	echo "$PUBLIC_SSH_KEY"
+	exit 1
+fi
+
 # Assign first argument to be Azure Resource Group
 GROUP=$1
-LOCATION=northeurope
 
-# Set sensible default
-if test -z $GROUP
- then
-  GROUP=ocp
+# Test group variable
+if test -z $GROUP; then
+	echo "Usuage: $0 <unique name for Azure resource group>"
+	exit 1
+else
+	cat azuredeploy.parameters.json|sed -e "s/REPLACE/$GROUP/g" -e "s/RHN_ACCOUNT/$RHN_ACCOUNT/" -e "s/RHN_PASSWORD/$RHN_PASSWORD/" -e "s/OCP_USER/$OCP_USER/" -e "s/OCP_PASSWORD/$OCP_PASSWORD/" -e "s/SUBSCRIPTION_POOL_ID/$SUBSCRIPTION_POOL/" >azuredeploy.parameters.json.new
+	mv azuredeploy.parameters.json.new azuredeploy.parameters.json
 fi
+
+echo "Deploying OpenShift Container Platform."
 
 # Create Azure Resource Group
 azure group create $GROUP $LOCATION
@@ -27,3 +75,8 @@ azure keyvault set-policy -u ${GROUP}KeyVaultName --enabled-for-template-deploym
 # Launch deployment of cluster, after this it’s just waiting for it to complete. 
 # azuredeploy.parameters.json needs to be populated with valid values first, before you run this.
 azure group deployment create --name ${GROUP} --template-file azuredeploy.json -e azuredeploy.parameters.json --resource-group $GROUP --nowait
+
+cat azuredeploy.parameters.json|sed -e "s/$GROUP/REPLACE/g" -e "s/$RHN_ACCOUNT/RHN_ACCOUNT/" -e "s/$RHN_PASSWORD/RHN_PASSWORD/" -e "s/$OCP_USER/OCP_USER/" -e "s/$OCP_PASSWORD/OCP_PASSWORD/" -e "s/$SUBSCRIPTION_POOL/SUBSCRIPTION_POOL_ID/" >azuredeploy.parameters.json.new
+mv azuredeploy.parameters.json.new azuredeploy.parameters.json
+
+echo "Deployment complete."
